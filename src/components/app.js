8d465cd01e4826aa2ed07621b425b778821ca2fb
calvinlfer <calvin.l.fer@outlook.com>
2017-04-11 19:09:28 -0400

    Use React Bootstrap to do a better navigation bar

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 export default class App extends Component {
-  render() {
+  navBarWithLogout() {
     return (
-      <div>
-          <h1>Weather Manager</h1>
-          {this.props.children}
-      </div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a>Weather Manager</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="/login">Logout</NavItem>
+          </Nav>
+        </Navbar>
     );
+  }
+
+  navBar() {
+      return (
+          <Navbar>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="/">Weather Manager</a>
+              </Navbar.Brand>
+            </Navbar.Header>
+          </Navbar>
+      );
+  }
+
+  render() {
+    const path = this.props.location.pathname;
+    if (path === '/login' || path === '/') {
+        return (
+            <div>
+                {this.navBar()}
+                {this.props.children}
+            </div>
+        );
+    } else {
+      return (
+          <div>
+              {this.navBarWithLogout()}
+              {this.props.children}
+          </div>
+      )
+    }
   }
 }
